86c1795dfc106f050d3491a4cdc99f33845f7e12
vvulfmann <joshua.grosser@gmail.com>
2019-03-28 20:36:00 -0400

    Initial commit.

---
+++ src/main/java/com/grosser/codesignal/CodesignalApplication.java
@@ -0,0 +1,13 @@
+package com.grosser.codesignal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodesignalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodesignalApplication.class, args);
+	}
+
+}
