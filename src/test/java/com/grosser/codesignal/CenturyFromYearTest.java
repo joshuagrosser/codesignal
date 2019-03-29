88b0be483a1b7430c4f6caab53534505d8d12f6a
vvulfmann <joshua.grosser@gmail.com>
2019-03-28 21:06:59 -0400

    Added CenturyFromYear and CheckPalindrome.

--- src/test/java/com/grosser/codesignal/CenturyFromYearTest.java
+++ src/test/java/com/grosser/codesignal/CenturyFromYearTest.java
@@ -7,22 +7,47 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertNotNull;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
 public class CenturyFromYearTest {
 
     @Autowired
-    CenturyFromYear instance;
+    CenturyFromYear centuryFromYear;
 
     @Test
-    public void centuryFromYear() {
-        assertTrue(true);
-        instance = new CenturyFromYear();
+    public void testNotNull() {
+        assertNotNull(centuryFromYear);
+    }
 
-        int centuryFromYear = instance.centuryFromYear(1);
-        assertEquals(1, centuryFromYear);
+    @Test
+    public void test1905() {
+        assertEquals(20, this.centuryFromYear.centuryFromYear(1905));
+    }
 
+    @Test
+    public void test1700() {
+        assertEquals(17, this.centuryFromYear.centuryFromYear(1700));
+    }
+
+    @Test
+    public void test2001() {
+        assertEquals(21, this.centuryFromYear.centuryFromYear(2001));
+    }
+
+    @Test
+    public void test200() {
+        assertEquals(2, this.centuryFromYear.centuryFromYear(200));
+    }
+
+    @Test
+    public void test374() {
+        assertEquals(4, this.centuryFromYear.centuryFromYear(374));
+    }
+
+    @Test
+    public void testNegativeNumber() {
+        assertEquals(-1, this.centuryFromYear.centuryFromYear(-100));
     }
 }
