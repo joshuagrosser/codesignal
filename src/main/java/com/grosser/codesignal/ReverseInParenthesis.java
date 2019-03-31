e2fcc3d6763c3fa7b6efeba2ce240f029b6ab926
vvulfmann <joshua.grosser@gmail.com>
2019-03-31 13:12:13 -0400

    Added ReverseInParenthesis.

---
+++ src/main/java/com/grosser/codesignal/ReverseInParenthesis.java
@@ -0,0 +1,49 @@
+package com.grosser.codesignal;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Write a function that reverses characters in (possibly nested) parentheses in the input string.
+ * <p>
+ * Input strings will always be well-formed with matching ()s.
+ * <p>
+ * Example
+ * <p>
+ * For inputString = "(bar)", the output should be
+ * reverseInParentheses(inputString) = "rab";
+ * For inputString = "foo(bar)baz", the output should be
+ * reverseInParentheses(inputString) = "foorabbaz";
+ * For inputString = "foo(bar)baz(blim)", the output should be
+ * reverseInParentheses(inputString) = "foorabbazmilb";
+ * For inputString = "foo(bar(baz))blim", the output should be
+ * reverseInParentheses(inputString) = "foobazrabblim".
+ * Because "foo(bar(baz))blim" becomes "foo(barzab)blim" and then "foobazrabblim".
+ */
+@Component
+public class ReverseInParenthesis {
+
+    String reverseInParentheses(String inputString) {
+        String result = inputString;
+        while(result.contains("(")) {
+            int innerStart = result.lastIndexOf("(");
+            int innerEnd = result.indexOf(")", innerStart);
+            String inner = result.substring(innerStart+1, innerEnd);
+            String reversed = reverse(inner);
+            result = result.replace("(" + inner + ")", reversed);
+        }
+        return result;
+    }
+
+    private String reverse(String inputString){
+        char[] chars = inputString.toCharArray();
+        char[] reversed = new char[chars.length];
+        int counter = 0;
+        for (int i = chars.length - 1; i >= 0; i--) {
+            reversed[counter] = chars[i];
+            counter++;
+        }
+        return String.valueOf(reversed);
+    }
+
+
+}
---
+++ src/test/java/com/grosser/codesignal/ReverseInParenthesisTest.java
@@ -0,0 +1,45 @@
+package com.grosser.codesignal;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class ReverseInParenthesisTest {
+
+    @Autowired
+    ReverseInParenthesis instance;
+
+    @Test
+    public void test001() {
+        assertEquals("abc321", instance.reverseInParentheses("abc(123)"));
+    }
+
+    @Test
+    public void test002() {
+        assertEquals("rab", instance.reverseInParentheses("(bar)"));
+    }
+
+    @Test
+    public void test003() {
+        assertEquals("foorabbaz", instance.reverseInParentheses("foo(bar)baz"));
+    }
+
+    @Test
+    public void test004() {
+        assertEquals("foorabbazmilb", instance.reverseInParentheses("foo(bar)baz(blim)"));
+    }
+
+    @Test
+    public void test005() {
+        assertEquals("foobazrabblim", instance.reverseInParentheses("foo(bar(baz))blim"));
+    }
+
+//
+
+}
