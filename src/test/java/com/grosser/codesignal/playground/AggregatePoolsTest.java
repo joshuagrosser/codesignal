87c133650fc8c21dc1cfdf7f87f1357eb75c384d
Joshua Grosser <joshua.grosser@gmail.com>
2019-07-05 08:55:58 -0400

    Cleanup.

--- src/test/java/com/grosser/codesignal/playground/AggregatePoolsTest.java
+++ src/test/java/com/grosser/codesignal/playground/AggregatePoolsTest.java
@@ -8,14 +8,14 @@ import static org.junit.Assert.*;
 
 public class AggregatePoolsTest {
 
-    @Test
-    public void test(){
-        int[] pools = new int[]{ 12000, 300, 500, 600, 700 };
-        int[][] result = AggregatePools.aggregatePools(pools);
-        for(int i = 0; i < result.length; i++){
-            System.out.println(Arrays.toString(result[i]));
-        }
-
-    }
+//    @Test
+//    public void test(){
+//        int[] pools = new int[]{ 12000, 300, 500, 600, 700 };
+//        int[][] result = AggregatePools.aggregatePools(pools);
+//        for(int i = 0; i < result.length; i++){
+//            System.out.println(Arrays.toString(result[i]));
+//        }
+//
+//    }
 
 }
